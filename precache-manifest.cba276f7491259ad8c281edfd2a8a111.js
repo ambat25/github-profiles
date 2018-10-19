6b5f12b0db5f75aa58d81eeeb5e65b2779e6e179
ambat25 <25ambat25@gmail.com>
2018-10-19 09:09:58 +0100

    Updates

---
+++ precache-manifest.cba276f7491259ad8c281edfd2a8a111.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "027d1ff8ddbc59af07be",
+    "url": "/github-profiles/static/js/runtime~main.027d1ff8.js"
+  },
+  {
+    "revision": "3ee4d883418f8755bc8c",
+    "url": "/github-profiles/static/js/main.3ee4d883.chunk.js"
+  },
+  {
+    "revision": "e8582f498abaf7db432c",
+    "url": "/github-profiles/static/js/1.e8582f49.chunk.js"
+  },
+  {
+    "revision": "3ee4d883418f8755bc8c",
+    "url": "/github-profiles/static/css/main.ed01bcec.chunk.css"
+  },
+  {
+    "revision": "e8582f498abaf7db432c",
+    "url": "/github-profiles/static/css/1.ba8d0cf5.chunk.css"
+  },
+  {
+    "revision": "f1b3403262fb33f40cd1d84437be0338",
+    "url": "/github-profiles/index.html"
+  }
+];
